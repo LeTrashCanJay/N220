d248247b5c83646045bb789bbf7ec95ed3db68cd
Jay Jones <jj147@iu.edu>
2023-09-12 19:56:23 -0400

    Homework 3 Complete. Couldn't get toFloat to work.

---
+++ homework-three/calculator.js
@@ -0,0 +1,8 @@
+function intFinal() {
+    var prin = document.getElementById('pInput').value;
+    var rat = document.getElementById('rInput').value;
+    var tim = document.getElementById('tInput').value;
+    var gained = (rat * tim)*100;
+    let Int = prin * (1+((rat/100)*tim));
+    document.getElementById('Interest').innerHTML = "With a starting principal of $" + prin + " and a growth rate of " + rat + "% over a period of " + tim + " years, your total interest will be $" + gained + " with a grand total of $" + Int + ".";
+}
---
+++ homework-three/readme.txt
@@ -0,0 +1,4 @@
+# Jay Jones
+## 9/6/2023
+### Homework 3
+WebLink: https://in-info-web4.informatics.iupui.edu/~jj147/N220/homework-three/InterestCalc.html
