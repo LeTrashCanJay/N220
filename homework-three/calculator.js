33193ad271df2e5984fa009090bdd6ef9d73e926
Jay Jones <jj147@iu.edu>
2023-09-12 20:18:00 -0400

    Added toFixed variables

--- homework-three/calculator.js
+++ homework-three/calculator.js
@@ -3,8 +3,8 @@ function intFinal() {
     var rat = document.getElementById('rInput').value;
     var tim = document.getElementById('tInput').value;
     var gained = (rat * tim)*100;
-    gained.toFixed(2);
+    let g = gained.toFixed(2);
     let Int = prin * (1+((rat/100)*tim));
-    Int.toFixed(2);
-    document.getElementById('Interest').innerHTML = "With a starting principal of $" + prin + " and a growth rate of " + rat + "% over a period of " + tim + " years, your total interest will be $" + gained + " with a grand total of $" + Int + ".";
+    let i = Int.toFixed(2);
+    document.getElementById('Interest').innerHTML = "With a starting principal of $" + prin + " and a growth rate of " + rat + "% over a period of " + tim + " years, your total interest will be $" + g + " with a grand total of $" + i + ".";
 }
