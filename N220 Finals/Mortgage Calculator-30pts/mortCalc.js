17b516f99813c72db30d0a13776df32f088807a2
Jay Jones <jj147@iu.edu>
2023-12-05 19:00:40 -0500

    Slightly off, but completed

--- N220 Finals/Mortgage Calculator-30pts/mortCalc.js
+++ N220 Finals/Mortgage Calculator-30pts/mortCalc.js
@@ -1,9 +1,9 @@
 function loanPay() {
    var l =document.getElementById('lInput').value;
-   var c =(document.getElementById('rInput').value/100);
+   var c =document.getElementById('rInput').value;
    var n =document.getElementById('tInput').value;
 
-   let payment = l*(c*((1+c)^(n*12)))/((1+c)^(n*12)-1);
+   let payment = (l * ((c/100) * Math.pow(1+(c/100), n))/(Math.pow(1+(c/100), n)-1))/12;
    let p = payment.toFixed(2);
    document.getElementById('amount').innerText="Loan Amount: $"+l;
    document.getElementById('rate').innerText="Interest Rate: "+c+"%";
